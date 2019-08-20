f65efd5f263916d3b548e90264bd3f56bd8ae5d5
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-08-20 14:22:25 +0100

    Array method ongoing

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,11 +58,20 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
+runners.forEach(
+  function(run){
+    fullNames.push(`${run.first_name} ${run.last_name}`)
+  }
+);
+
 console.log(fullNames);
+
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
+
+// console.log(colors.map(a => a.toUpperCase()));
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
