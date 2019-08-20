f7b632de207d2ae4969ea593f6e2721d007a2539
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-08-20 13:54:45 +0100

    Call back challenge done (-1)

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,9 +2,9 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
-  // GIVEN THIS PROBLEM:
+/*
+  //GIVEN THIS PROBLEM:
 
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
@@ -41,24 +41,57 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+function logArrayLength(testis){
+  console.log(`The length of this array is ${testis}`)
+};
+getLength(items,logArrayLength);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb (arr[arr.length - 1]);
 }
+function lastItem(test2){
+  console.log(`The last item of the array is ${test2}`);
+};
+last(items,lastItem);
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let add = x + y;
+  return cb(add); 
 }
+sumNums(5,6, console.log);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let mul = x * y;
+  return cb(mul); 
 }
+multiplyNums(10,6, console.log);
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  //var arr = [1, 2, 3];
+// var evens = arr.filter((num) => {
+//   if (num % 2 === 0) {
+//     return true;
+//   }
+//   return false;
+// });
+  for (let i=0;i<list.length;i++){
+    if(list[i]===item){
+      result=true;
+    }
+    else result = false;
+  }
+  return cb(result);
 }
+
 
 /* STRETCH PROBLEM */
 
